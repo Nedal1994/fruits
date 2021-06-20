4362298b7480636533d8ee666dabb5b78b6de3c2
Nedal1994 <>
2021-06-20 08:08:32 +0000

    update

---
+++ app.js
@@ -0,0 +1,25 @@
+var name = prompt("What's your name?")
+alert("Welcome to fruit website " + name)
+
+var favfruit = prompt("What is your favorite fruit ?(apple, pomegranate, orange)?")		
+
+while(favfruit != "apple" && favfruit != "pomegranate" && favfruit != "orange")
+{
+  favfruit = prompt("You inserted invalid fruit. Please try again")
+}
+
+if(favfruit == "apple")
+{document.write("<p>"+ "It is available" +"</p>")}	
+
+else if(favfruit == "pomegranate"){document.write("<p>"+ "It is available"+"</p>")} 
+
+else if(favfruit == "orange"){document.write("<p>"+ "It is available" +"</p>")}		  
+
+else 
+{ alert("Not available") }
+
+var fruit = prompt("How much do you want ?")
+for(i=0;i<=10;i++)
+{
+console.log(favfruit + " is available")
+}
